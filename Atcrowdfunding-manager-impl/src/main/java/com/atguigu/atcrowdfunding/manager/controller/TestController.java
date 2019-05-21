68736b220c2325342260ad6d599f94db7671a0b9
tom_glb <goodMorning_glb@mayan.com>
2019-05-21 12:23:37 +0800

    commit

---
+++ Atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/manager/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.atguigu.atcrowdfunding.manager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.atguigu.atcrowdfunding.manager.service.TestService;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	private TestService testService ;
+	
+	@RequestMapping("/test")
+	public String test(){
+		System.out.println("TestController");
+		
+		testService.insert();
+		
+		return "success";
+	}
+	
+}
